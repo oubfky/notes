cff9f9065e4ee664f74b096baf1c014edd71794a
oubfky <luping53@gmail.com>
2022-11-14 18:01:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+print("Hello World")
+print("     /|")
+print("    / |")
+print("   /  |")
+print("  /___|")
+
+character_name = "Mike"
+character_age = "30"
+is_male = False
+print("There once was a man named " + character_name)
+print("He was " + character_age + " years old.")
+print("He really liked the name " + character_name)
+print("but didn't like being " + character_age + ".")
+
+print("Giraffe Academy")
+print("Giraffe\nAcademy")
+print("Giraffe\“Academy")
+print("Giraffe\Academy")
+
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print(phrase.lower())  # lower case
+print(phrase.upper())  # upper case
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))  # length of the string
+print(phrase[0])  # the first character
+print(phrase[3])  # a
+print(phrase.index("G"))  # 0, the index of G
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+
+print(3+4.5)
+print(-2.4)
+print(3/2)
+print(3*5)
+print(3*(4+5))
+print(10%3) #mod 1
+
+my_num = 5
+print(my_num)
+print(str(my_num))#convert a number to a string
+print(str(my_num) + " my favorite number")
+
+my_num = -5
+print(abs(my_num))
+print(pow(3,2)) # 3*3
+print(max(4,6))
+print(min(4,6))
+print(round(5.3))
+
+from math import *
+print(floor(5.1))
+print(ceil(3.7))
+print(sqrt(36))
+
